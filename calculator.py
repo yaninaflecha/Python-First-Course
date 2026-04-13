f294f960ecaf985f3f2e5e7c9b491e8044b094c2
Yanina Flecha <yxymxi@gmail.com>
2026-04-13 12:04:04 -0300

    Some more examples added

--- calculator.py
+++ calculator.py
@@ -1,12 +1,26 @@
-a = input("What's a value? ")
-b= input("What's b value?")
-c = int(a) + int(b)
-print(c)
+a = float(input("What's a value? "))
+
+b= float(input("What's a value? "))
+
+c = a / b
+
+#c= round(a / b,2) 1st way to round
+
+print(f"{c:.2f}") #same thing as rounding first
+#and print after
+
+#I'm gonna comment some lines to practice / 
+
+#c = round(a+b,1)
+
+#print (f"{c:,}")
+
+# or print(round(c,1))
+  
 
 
 #but i can obtain the same result doing it like:
-""""""
-a = int(input("What's a value? "))
-b = int(input("What's b value? "))
-print(a+b)
-""""""
+
+#a = int(input("What's a value? "))
+#b = int(input("What's b value? "))
+#print(a+b)
