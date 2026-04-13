11ffa3bcc988c9f693484d0d5eae9701cc426da7
Yanina Flecha <yxymxi@gmail.com>
2026-04-13 00:07:28 -0300

    First class finished

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("hello, world")
+#This is intended to ask the users name 
+#Then remove whitespace from str andCapitalize user's name
+
+name =input("What's your name?: ").strip().title()
+
+#And hen say them hello
+
+print("hello", name)
+
+#I can also solve it this way
+#print(f"hello {name}")
+#Style depends a lot on what you like
+#BUt also what the company you work for expects
+
