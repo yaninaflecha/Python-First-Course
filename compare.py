4f646ed4b589fa698546499e10167d40053a3bf7
Yanina Flecha <yxymxi@gmail.com>
2026-04-17 18:49:53 -0300

    New content added

--- compare.py
+++ compare.py
@@ -2,9 +2,15 @@ x = int(input("What's x? "))
 y = int(input("What's y?"))
 
 if x < y:
-    print("x is less than y")
-if y < x:
-    print("y is less than x")
-if y== x:
+    print(x, " is less than", y)
+elif y < x:
+    print(x," is less than ",y)
+else:
     print("They are the same")
 
+
+
+if x != y: 
+    print("X is not equal to Y")
+else: 
+    print("X is Equal to Y")
---
+++ parity.py
@@ -0,0 +1,32 @@
+def main():
+    x = int(input("What's x?: "))
+    if is_odd(x):
+        print(x, "is odd")
+    else:
+        print(x, "is even")
+
+def is_odd(n):
+        return n%2
+
+
+main() 
+#If i do it this way it works because for ex. 
+#2/2= 0 and 0=False for computers
+
+
+def main():
+    x = int(input("What's x?: "))
+    if is_even(x):
+        print(x, "is even")
+    else:
+        print(x, "is odd")
+   
+    
+def is_even(n):
+        if n%2 ==0:
+             return True
+        else: 
+             return False
+
+
+main()
