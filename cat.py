7adbb0e291ea60977a3fa1b544ce9edc1a0532c6
Yanina Flecha <yxymxi@gmail.com>
2026-04-18 05:40:13 -0300

    Loop class started

---
+++ cat.py
@@ -0,0 +1,19 @@
+
+
+for meow in range(3):
+    print("Meow")
+print("Finished.")
+#This is the way I find out it can be done
+
+cat= 0
+while  cat<3:
+    print("Meow")
+    cat+=1
+print("Done.")
+
+#Or it can be done as
+i=3
+while i!=0:
+    print("Meow")
+    i-=1
+print("It's done.")
