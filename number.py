4b1ee857ee1c3f650d085121bc6cf9774259c825
Yanina Flecha <yxymxi@gmail.com>
2026-04-21 11:38:19 -0300

    Made a function for the class

--- number.py
+++ number.py
@@ -1,9 +1,17 @@
-while True:
-    try:
-        x = int(input("What's x?: "))
-    except ValueError:
-        print(" x is meant to be a number.")
-    else:
-        break
-print("x is", x)
+def main():
+    x= get_int()
+    print()
+    print("x is", x)
+
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x?: "))
+        except ValueError:
+            print(" x is meant to be a number.")
+        else:
+            break
+    return x
     
+main() 
