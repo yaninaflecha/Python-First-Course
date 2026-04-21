205ea0b9f09f41db3691746baf51276d6184511a
Yanina Flecha <yxymxi@gmail.com>
2026-04-21 11:41:00 -0300

    Made a function for the class and shortened the code

--- number.py
+++ number.py
@@ -1,17 +1,15 @@
 def main():
     x= get_int()
-    print()
     print("x is", x)
 
 
 def get_int():
     while True:
         try:
-            x = int(input("What's x?: "))
+            return int(input("What's x?: "))
+            
         except ValueError:
             print(" x is meant to be a number.")
-        else:
-            break
-    return x
+    
     
 main() 
