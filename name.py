aad394408a1f64bf1cecff9cf235fa4e30a930c4
Yanina Flecha <yxymxi@gmail.com>
2026-04-27 11:18:13 -0300

    Started learning about packages

--- name.py
+++ name.py
@@ -1,11 +1,16 @@
 #sys.argv is a list, recheck later
 import sys
 
+#Check for errors
 if len(sys.argv) <2:
-    print("Too few arguments")
-    
-elif len(sys.argv)>2:
-    print("Too many arguments")
+    sys.exit("Too few arguments") #sys.exit works to prevent the program to continue when we get in the exception
 
-else:
-    print("hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]: #Doing this I can have a slice from a list of elements to prevent printing "name.py"
+    print("Hello, my name is", arg)
+#elif len(sys.argv)>2:
+    #sys.exit("Too many arguments")
+
+#Print name tags
+#print("hello, my name is", sys.argv[1])
+
+#You can have multiple elif but not multiple else statements
