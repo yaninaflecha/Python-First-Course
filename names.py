08e6c59ab7edf456f465969c55bd321d75f5dae1
Yanina Flecha <yxymxi@gmail.com>
2026-05-07 10:58:27 -0300

    Started I/O chapter

--- names.py
+++ names.py
@@ -1,8 +1,27 @@
+#Method 1.
 
-names = []
-for _ in range(3):
-    name = input("What's your name? ")
-    names.append(name)
+#names = []
+#for _ in range(3):
+    #name = input("What's your name? ")
+    #names.append(name)
 
-for name in sorted(names):
-    print(f"Hello, {name}")
+#for name in sorted(names):
+    #print(f"Hello, {name}")
+
+#Method 2:
+#name = input("What's your name? ")
+#with open("names.txt", "a") as file:
+    #file.write(f"{name}\n")
+
+#Method 1 to read the file:
+with open("names.txt") as file:
+    for line in sorted(file, reverse= True):
+        print("hello, ", line.rstrip())
+#Method 2:
+#names = []
+#with open("names.txt") as file:
+    #for line in file:
+        #names.append(line.rstrip())
+#for name in sorted(names):
+    #print(f"hello, {name}")
+
